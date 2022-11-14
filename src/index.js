f644545e43f3c68ba8e498ec21211a17d90df07a
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-14 11:57:24 -0800

    added ui

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import { Person } from './js/person.js';
 // UI Logic
 
 const handleSubmission = (event) => {
-
-}
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  const lifeExp = document.getElementById("life-exp").value;
+  window.person = new Person(name, age, lifeExp);
+};
 
 window.addEventListener("load", function () {
-
-})
+  this.document.getElementById("person").addEventListener("submit", handleSubmission);
+});
