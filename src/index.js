301fde7a1c242b95f1c832b93df6f2ea61a2c2f1
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-14 14:28:13 -0800

    added indiv planet buttons and mercury func

--- src/index.js
+++ src/index.js
@@ -4,21 +4,50 @@ import './css/styles.css';
 import { Person } from './js/person.js';
 
 // UI Logic
-
-const handleSubmission = (event) => {
-  event.preventDefault();
+const createPerson = () => {
   const name = document.getElementById("name").value;
   const age = document.getElementById("age").value;
   const lifeExp = document.getElementById("life-exp").value;
-  window.person = new Person(name, age, lifeExp);
+  window.personObj = new Person(name, age, lifeExp);
+};
 
-  document.getElementById("response").innerText = "Age: \n" +
-  "on Mercury: " + window.person.onMercury() + "\n"+
-  "on Venus: " + window.person.onVenus() + "\n" +
-  "on Mars: " + window.person.onMars() + "\n" +
-  "on Jupiter: " + window.person.onJupiter() + "\n";
+const clearResponse = () => { 
+  document.getElementById("response").innerText = null;
+};
+
+const handleMerc = () => {
+  clearResponse();
+  createPerson();
+  const h3 = document.createElement("h3");
+  h3.append("Age on Mercury: ");
+  const p = document.createElement("p");
+  p.append(window.personObj.onMercury());
+  document.getElementById("response").append(h3);
+  document.getElementById("response").append(p);
+};
+
+const handleVen = () => {
+  clearResponse();
+  createPerson();
+
+};
+
+const handleMars = () => {
+  clearResponse();
+  createPerson();
+
+};
+
+const handleJup = () => {
+  clearResponse();
+  createPerson();
+
 };
 
 window.addEventListener("load", function () {
-  this.document.getElementById("person").addEventListener("submit", handleSubmission);
+  this.document.getElementById("btn-merc").addEventListener("click", handleMerc);
+  this.document.getElementById("btn-ven").addEventListener("click", handleVen);
+  this.document.getElementById("btn-mars").addEventListener("click", handleMars);
+  this.document.getElementById("btn-jup").addEventListener("click", handleJup);
+
 });
