8b2fc20483aac795151240ec825009d6ce27dd98
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-14 16:27:12 -0800

    added functionality to years left

--- src/index.js
+++ src/index.js
@@ -15,33 +15,73 @@ const clearResponse = () => {
   document.getElementById("response").innerText = null;
 };
 
-const handleSubmission = (planet) => {
+const handleSubmission = (planet, left) => {
   clearResponse();
   createPerson();
   const h3 = document.createElement("h3");
-  h3.append("Age on " + planet + ": ");
   const p = document.createElement("p");
-  switch (planet) {
-  case "Mercury":
-    p.append(window.personObj.onMercury());
-    break;
-  case "Venus":
-    p.append(window.personObj.onVenus());
-    break;
-  case "Mars":
-    p.append(window.personObj.onMars());
-    break;
-  case "Jupiter":
-    p.append(window.personObj.onJupiter());
-    break;
-  }
 
+  if (!left) {
+    h3.append("Age on " + planet + ": ");
+    switch (planet) {
+    case "Mercury":
+      p.append(window.personObj.onMercury());
+      break;
+    case "Venus":
+      p.append(window.personObj.onVenus());
+      break;
+    case "Mars":
+      p.append(window.personObj.onMars());
+      break;
+    case "Jupiter":
+      p.append(window.personObj.onJupiter());
+      break;
+    }
+  } else {
+    h3.append("Years left on " + planet + ": ");
+    switch (planet) {
+    case "Mercury":
+      p.append(window.personObj.onMercuryLeft());
+      break;
+    case "Venus":
+      p.append(window.personObj.onVenusLeft());
+      break;
+    case "Mars":
+      p.append(window.personObj.onMarsLeft());
+      break;
+    case "Jupiter":
+      p.append(window.personObj.onJupiterLeft());
+      break;
+    }
+  }
   document.getElementById("response").append(h3);
   document.getElementById("response").append(p);
 };
 
+
 window.addEventListener("load", function () {
-  this.document.getElementById("btn-merc").addEventListener("click", function (){
-    handleSubmission("Mercury");
+  this.document.getElementById("btn-merc").addEventListener("click", function () {
+    handleSubmission("Mercury", false);
+  });
+  this.document.getElementById("btn-ven").addEventListener("click", function () {
+    handleSubmission("Venus", false);
+  });
+  this.document.getElementById("btn-mars").addEventListener("click", function () {
+    handleSubmission("Mars", false);
+  });
+  this.document.getElementById("btn-jup").addEventListener("click", function () {
+    handleSubmission("Jupiter", false);
+  });
+  this.document.getElementById("btn-merc-left").addEventListener("click", function () {
+    handleSubmission("Mercury", true);
+  });
+  this.document.getElementById("btn-ven-left").addEventListener("click", function () {
+    handleSubmission("Venus", true);
+  });
+  this.document.getElementById("btn-mars-left").addEventListener("click", function () {
+    handleSubmission("Mars", true);
+  });
+  this.document.getElementById("btn-jup-left").addEventListener("click", function () {
+    handleSubmission("Jupiter", true);
   });
 });
