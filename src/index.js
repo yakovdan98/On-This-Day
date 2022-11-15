f45eaf0a91abf32c29d90ee9b220f88f6c824c34
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-11-14 16:15:26 -0800

    refactored ui logic to one function

--- src/index.js
+++ src/index.js
@@ -11,58 +11,37 @@ const createPerson = () => {
   window.personObj = new Person(name, age, lifeExp);
 };
 
-const clearResponse = () => { 
+const clearResponse = () => {
   document.getElementById("response").innerText = null;
 };
 
-const handleMerc = () => {
+const handleSubmission = (planet) => {
   clearResponse();
   createPerson();
   const h3 = document.createElement("h3");
-  h3.append("Age on Mercury: ");
+  h3.append("Age on " + planet + ": ");
   const p = document.createElement("p");
-  p.append(window.personObj.onMercury());
-  document.getElementById("response").append(h3);
-  document.getElementById("response").append(p);
-};
+  switch (planet) {
+  case "Mercury":
+    p.append(window.personObj.onMercury());
+    break;
+  case "Venus":
+    p.append(window.personObj.onVenus());
+    break;
+  case "Mars":
+    p.append(window.personObj.onMars());
+    break;
+  case "Jupiter":
+    p.append(window.personObj.onJupiter());
+    break;
+  }
 
-const handleVen = () => {
-  clearResponse();
-  createPerson();
-  const h3 = document.createElement("h3");
-  h3.append("Age on Venus: ");
-  const p = document.createElement("p");
-  p.append(window.personObj.onVenus());
-  document.getElementById("response").append(h3);
-  document.getElementById("response").append(p);
-};
-
-const handleMars = () => {
-  clearResponse();
-  createPerson();
-  const h3 = document.createElement("h3");
-  h3.append("Age on Mars: ");
-  const p = document.createElement("p");
-  p.append(window.personObj.onMars());
-  document.getElementById("response").append(h3);
-  document.getElementById("response").append(p);
-};
-
-const handleJup = () => {
-  clearResponse();
-  createPerson();
-  const h3 = document.createElement("h3");
-  h3.append("Age on Jupiter: ");
-  const p = document.createElement("p");
-  p.append(window.personObj.onJupiter());
   document.getElementById("response").append(h3);
   document.getElementById("response").append(p);
 };
 
 window.addEventListener("load", function () {
-  this.document.getElementById("btn-merc").addEventListener("click", handleMerc);
-  this.document.getElementById("btn-ven").addEventListener("click", handleVen);
-  this.document.getElementById("btn-mars").addEventListener("click", handleMars);
-  this.document.getElementById("btn-jup").addEventListener("click", handleJup);
-
+  this.document.getElementById("btn-merc").addEventListener("click", function (){
+    handleSubmission("Mercury");
+  });
 });
